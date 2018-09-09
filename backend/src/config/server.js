3f6531a96b876148f038d76d24583e3401ac6786
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-09-08 23:39:08 -0300

    98. Habilitando o CORS

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const allowCors = require('./cors')
 
 const port = 3003
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, () => {
   console.log(`BACKEND is running on port ${ port }`)
