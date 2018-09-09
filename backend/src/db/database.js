648c7f3a8332cb35b65ee7a6136466c92ff77921
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-09-08 23:17:45 -0300

    97. Tratando as Mensagens de Erro

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -8,8 +8,8 @@ module.export = conn
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
-  "O {VALUE} informado é menor que o limite mínimo de {MIN}"
+  "O valor {VALUE} informado no campo {PATH} é menor que o limite mínimo de {MIN}"
 mongoose.Error.messages.Number.max = 
-  "O {VALUE} informado é maior que o limite máximo de {MAX}"
+  "O valor {VALUE} informado no campo {PATH} é maior que o limite máximo de {MAX}"
 mongoose.Error.messages.String.enum = 
-  "O {VALUE} não é válido para o atributo {PATH}"
+  "O valor {VALUE} não é válido para o atributo {PATH}"
