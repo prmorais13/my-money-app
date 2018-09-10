3771097ffdb8434016c0137e1bf028d94d343633
unknown <Paulo@TI01.pgmnet.local>
2018-09-10 16:09:48 -0300

    106. Dependências do Template (AdminLTE)

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import '../common/template/dependencies'
+
 export default props => (
   <div className="wrapper">
     <h1>App</h1>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx'],
     alias: {
-      modules: __dirname + './node_modules',
-      jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+      modules: __dirname + '/node_modules',
+      jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.1.4.min.js',
       bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
     }
   },
