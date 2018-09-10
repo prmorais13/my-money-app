43ae9754b939e0940ff6ffdf59888515931a08d7
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-09-10 20:59:32 -0300

    110. Componente Rodapé

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy 2018
+      <a href="http://www.natal.rn.gov.br" 
+          target="_blank"> Prefeitura de Natal</a>.
+    </strong>
+  </footer>
+)
