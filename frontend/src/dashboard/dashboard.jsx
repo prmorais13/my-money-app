6777d7fd009a78836f32bca170c38862f437f69d
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-09-11 19:21:56 -0300

    121. Configurando Redux DevTools

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
+import { getSummary } from './dashboardAction'
 
 class Dashboard extends Component {
+
+  componentWillMount() {
+    this.props.getSummary()
+  }
+
   render() {
-    const { credit, debt } = this.props.summary
+    const { credit, debt } = this.props.summary 
     return (
       <div>
         <ContentHeader title="Dashboard" subtitle="Versão 1.0"/>
@@ -17,7 +24,7 @@ class Dashboard extends Component {
             <ValueBox cols="12 4" color="green" icon="bank" 
                 value={`R$ ${credit}`} text="Total de créditos"/>
 
-            <ValueBox cols="12 4" color="red" icon="credit-card" 
+            <ValueBox cols="12 4" color="red" icon="credit-card"
                 value={`R$ ${debt}`} text="Total de Débitos"/>
 
             <ValueBox cols="12 4" color="blue" icon="money" 
@@ -30,5 +37,5 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => ({ summary: state.dashboard.summary })
-
-export default connect(mapStateToProps) (Dashboard)
+const mapDispatchToProps = dispatch => bindActionCreators({ getSummary }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- frontend/src/dashboard/dashboardAction.js
+++ frontend/src/dashboard/dashboardAction.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const BASE_URL = 'http://localhost:3003/api'
 
-export function getSumary() {
-  const request = axios.get(`${BASE_URL}/billingCycles/summary`)
+export function getSummary() {
+  const request = axios.get(`${BASE_URL}/billingCycles/sumary`)
   return {
     type: 'BILLING_SUMMARY-FETCHED',
     payload: request
